710c19564d793bf00310c11f8cb413ecfbe68f50
Greg Heartsfield <scsibug@imap.cc>
2011-06-08 20:13:04 -0500

    Removed guiceberry dependency (broken with JUnit4).

--- src/main/java/com/gregheartsfield/revelation/ChangeLog.java
+++ src/main/java/com/gregheartsfield/revelation/ChangeLog.java
@@ -1,6 +1,28 @@
 package com.gregheartsfield.revelation;
 
 import java.util.Vector;
+import com.google.inject.Inject;
 
-public class ChangeLog extends Vector<ChangeSet> {
+/**
+ *  Ordered list of changesets
+ */
+
+public class ChangeLog {
+    Vector<ChangeSet> log = new Vector<ChangeSet>();
+    
+    /*
+    @Inject
+    public ChangeSet newChangeSet(ChangeSet cs) {
+        log.add(cs);
+        return cs;
+    }
+    */
+
+    public int size() {
+        return log.size();
+    }
+
+    public ChangeSet elementAt(int i) {
+        return log.elementAt(i);
+    }
 }
--- src/test/java/com/gregheartsfield/revelation/ChangeLogTest.java
+++ src/test/java/com/gregheartsfield/revelation/ChangeLogTest.java
@@ -1,24 +1,35 @@
 package com.gregheartsfield.revelation;
 
+import static org.junit.Assert.assertTrue;
+
+import com.google.inject.Guice;
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+
 import org.junit.*;
-import static org.junit.Assert.*;
 
 public class ChangeLogTest {
     ChangeLog cl;
+    Injector injector;
 
     @Before
     public void setup() {
-        cl = new ChangeLog();
+        injector = Guice.createInjector(new Env());
     }
 
     @After
     public void tearDown() {
         cl = null;
+        injector = null;
     }
 
     @Test
-    public void empty() {
-        assertEquals("ChangeLog starts empty", 0, cl.size());
+    public void testNothing() throws Exception {
+        assertTrue(true);
     }
 
+    public class Env extends AbstractModule{
+        protected void configure() {
+        }
+    }
 }
