c34b8ab952720542ca374f80107c6154ee8d7e31
Greg Heartsfield <scsibug@imap.cc>
2011-06-09 00:53:57 -0500

    Added dependencies for logging with sl4j.

--- src/main/java/com/gregheartsfield/revelation/App.java
+++ src/main/java/com/gregheartsfield/revelation/App.java
@@ -3,10 +3,17 @@ package com.gregheartsfield.revelation;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App
 {
+
+
     public static void main( String[] args )
     {
+        final Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Creating guice injector");
         Injector injector = Guice.createInjector(new RevelationModule());
         RevelationServer revServ = injector.getInstance(RevelationServer.class);
         revServ.run();
