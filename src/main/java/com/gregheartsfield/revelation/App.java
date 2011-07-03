6babf9e516bd1d953fbf3bbc94da2f846bad3b22
Greg Heartsfield <scsibug@imap.cc>
2011-07-02 20:28:56 -0500

    Whitespace.

--- src/main/java/com/gregheartsfield/revelation/App.java
+++ src/main/java/com/gregheartsfield/revelation/App.java
@@ -6,16 +6,12 @@ import com.google.inject.Injector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class App
-{
-
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         final Logger logger = LoggerFactory.getLogger(App.class);
         logger.info("Creating guice injector");
         Injector injector = Guice.createInjector(new RevelationModule());
         RevelationServer revServ = injector.getInstance(RevelationServer.class);
         revServ.run();
     }
-
 }
